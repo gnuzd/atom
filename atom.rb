fd2909f0601e325693a3557c88019256c04546b2
Chris Nguyen <chris.dungnguyen@gmail.com>
2026-04-07 23:21:29 +0700

    chore: update SHA256 in formula for release v0.1.0

--- atom.rb
+++ atom.rb
@@ -2,7 +2,7 @@ class Atom < Formula
   desc "A lightning-fast, modal terminal editor written in Rust"
   homepage "https://github.com/gnuzd/atom"
   url "https://github.com/gnuzd/atom/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "36ae46b42f0d6dc19ab6a69359046f278c77c8bbbc43527be3f247788a3fd24d"
   license "MIT"
 
   depends_on "rust" => :build
